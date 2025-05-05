3b82a96824b299104ddcea578d57fc3ffd6734fc
CHZelaya <chzelaya1201@gmail.com>
2025-05-05 15:41:46 -0600

    [feat] Created Callback.vue page to handle the start of the token exchange with AWS Cognito

--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,5 +19,6 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/Callback': RouteRecordInfo<'/Callback', '/Callback', Record<never, never>, Record<never, never>>,
   }
 }
